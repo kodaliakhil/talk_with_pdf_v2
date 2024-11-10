22915a13e2633240d41913a6a14aed0b9f933a7f
kodaliakhil <kodaliakhil1999@gmail.com>
2024-11-10 17:59:11 +0530

    file storage in convex DB

--- app/dashboard/_components/UploadPdfDialog.js
+++ app/dashboard/_components/UploadPdfDialog.js
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import {
   Dialog,
   DialogClose,
@@ -11,8 +12,49 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Loader2Icon } from "lucide-react";
+import { v4 as uuidv4 } from "uuid";
+import { useUser } from "@clerk/nextjs";
 
 const UploadPdfDialog = ({ children }) => {
+  const generateUploadUrl = useMutation(api.fileStorage.generateUploadUrl);
+  const addFileEntry = useMutation(api.fileStorage.addFileEntryToDb);
+  const getFileUrl = useMutation(api.fileStorage.getFileUrl);
+  const { user } = useUser();
+  const [file, setFile] = useState();
+  const [loading, setLoading] = useState(false);
+  const [fileName, setFileName] = useState();
+  const OnFileSelect = (event) => {
+    setFile(event.target.files[0]);
+  };
+  const OnUpload = async () => {
+    setLoading(true);
+    // Step 1: Get a short-lived upload URL
+    const postUrl = await generateUploadUrl();
+
+    // Step 2: POST the file to the URL
+    const result = await fetch(postUrl, {
+      method: "POST",
+      headers: { "Content-Type": file?.type },
+      body: file,
+    });
+    const { storageId } = await result.json();
+    console.log("storageId", storageId);
+    const fileId = uuidv4();
+    const fileUrl = await getFileUrl({ storageId });
+    // Step 3: Save the newly allocated storage id to the database
+    const resp = await addFileEntry({
+      fileId,
+      storageId,
+      fileName: fileName ?? "Untitled File",
+      fileUrl,
+      createdBy: user?.primaryEmailAddress?.emailAddress,
+    });
+    console.log(resp);
+    setLoading(false);
+  };
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
@@ -23,11 +65,18 @@ const UploadPdfDialog = ({ children }) => {
             <div className="">
               <h2 className="mt-5">Select a file to upload</h2>
               <div className="gap-2 p-3 rounded-md border">
-                <input type="file" accept="application/pdf" />
+                <input
+                  type="file"
+                  accept="application/pdf"
+                  onChange={(event) => OnFileSelect(event)}
+                />
               </div>
               <div className="mt-2">
                 <label>File Name *</label>
-                <Input placeholder="File Name" />
+                <Input
+                  placeholder="File Name"
+                  onChange={(e) => setFileName(e.target.value)}
+                />
               </div>
             </div>
           </DialogDescription>
@@ -38,7 +87,9 @@ const UploadPdfDialog = ({ children }) => {
               Close
             </Button>
           </DialogClose>
-          <Button>Upload</Button>
+          <Button onClick={OnUpload}>
+            {loading ? <Loader2Icon className="animate-spin" /> : "Upload"}
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
